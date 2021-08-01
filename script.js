765e5e5b082bcda96ddae248c7365dbe557e5c38
eliwangila <ekirapaeli254@gmail.com>
2021-08-01 08:49:29 +0300

    add prototype function

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-// pizza object
+// pizza object use of this keyword
 function Pizza(size, crust, topping) {
     this.size = size;
     this.crust = crust;
     this.topping = topping;
     this.price = 0;
 }
+
+
+//fuction to call the totalCost of pizza onces selected 
+Pizza.prototype.totalCost = function () {
+    switch (this.size) {
+        case "small":
+            this.price = 800;
+            break;
+        case "medium":
+            this.price = 1000;
+            break;
+        case "large":
+            this.price = 1200;
+            break;
+        default:
+            break;
+    }
+    switch (this.crust) {
+        case "crust-crispy":
+            this.price += 80;
+            break;
+        case "crust-stuffed":
+            this.price += 80;
+            break;
+        case "sugar-free":
+            this.price += 80;
+            break;
+        default:
+            break;
+    }
+};
